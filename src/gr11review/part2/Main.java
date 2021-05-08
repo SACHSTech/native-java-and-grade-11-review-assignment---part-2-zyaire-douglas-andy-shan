2857587ed67980e60bc594a8cdc1700b3fe95521
Douglas Lau <>
2021-05-08 16:07:24 +0000

    Methods 1

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,22 @@
 package gr11review.part2;
 
+import java.io.*;
+import gr11review.part2.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+      
+     // use this area to test methods from Utility.java
+     // create objects
+     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+     // declare variables
+     String str;
+
+     // ask for user input of words
+     str = keyboard.readLine();
+
+     // use method to zipZap words
+     System.out.print(Utility.zipZap(str));
+    }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,27 @@
 package gr11review.part2;
+
 import java.io.*;
+import java.util.Scanner;
 
 public class Utility {
 
-    
+// Methods 1
+public static String zipZap(String str) {
 
-    
+  // declare variables
+  String zapStr = "";
+
+  // zipZap letter between "z" and "p" 
+  for (int i = 0; i < str.length(); i++) {
+    if ((i < str.length() - 2) && (str.substring(i, i + 1).equals("z")) && (str.substring(i + 2, i + 3).equals("p"))) {
+      zapStr += "zp";
+      i += 2;
+    }
+    else
+     zapStr += str.substring(i, i + 1);
+  }
+
+  // return the zapped string
+  return zapStr;
+}
 }
