a42c4ed5e438c61666d54912cdad6361991222c1
Douglas Lau <>
2021-05-10 04:29:53 +0000

    Main.java Updates

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -6,17 +6,27 @@ import gr11review.part2.*;
 public class Main {
     public static void main(String[] args) throws IOException {
       
-     // use this area to test methods from Utility.java
-     // create objects
-     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+     // Methods 1
+     String str = "zipXzap";
+     System.out.println(Utility.zipZap(str));
 
-     // declare variables
-     String str;
+     // FilIO - Read 1
+     System.out.println(Utility.longestWord("src/gr11review/test2/Review2_3Test_1.txt"));
 
-     // ask for user input of words
-     str = keyboard.readLine();
+     // Array 1
+     int[] nums = {2, 10, 3, 4, 20, 5};
+     nums = Utility.tenRun(nums);
+     System.out.println(Arrays.toString(nums));
 
-     // use method to zipZap words
-     System.out.print(Utility.zipZap(str));
+     // Array 3
+     int[] outer = {1, 2, 4, 6};
+     int[] inner = {2, 4};
+
+     System.out.println(Utility.linearIn(outer, inner));
+
+     // Array 5
+     int i = 1;
+     int j = 2;
+     System.out.println(Utiliyt.pascalTri(i, j));
     }
 }
